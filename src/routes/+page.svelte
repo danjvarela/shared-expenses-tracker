b5dea19d674e2a3ada7d499f6d474a9771ba1ae9
Dan <vareladanmarj@gmail.com>
2026-08-01 19:20:44 +0800

    initialize shadcn and add button component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import ArrowUpIcon from '@lucide/svelte/icons/arrow-up';
+	import { Button } from '$lib/components/ui/button/index.js';
+</script>
+
+<div class="flex flex-wrap items-center gap-2 md:flex-row">
+	<Button variant="outline">Button</Button>
+	<Button variant="outline" size="icon" aria-label="Submit">
+		<ArrowUpIcon />
+	</Button>
+</div>
