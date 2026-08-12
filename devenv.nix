5fd8916f73c1749214fa64fbcb49db85a0b9cfa6
Dan <vareladanmarj@gmail.com>
2026-08-13 02:58:33 +0800

    feat(dev): setup secretspec

--- devenv.nix
+++ devenv.nix
@@ -11,4 +11,8 @@
 
   env.PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD = "1";
   env.PLAYWRIGHT_BROWSERS_PATH = "${pkgs.playwright-driver.browsers}";
+
+  env.DATABASE_URL = config.secretspec.secrets.DATABASE_URL or "";
+  env.GOOGLE_CLIENT_ID = config.secretspec.secrets.GOOGLE_CLIENT_ID or "";
+  env.GOOGLE_CLIENT_SECRET = config.secretspec.secrets.GOOGLE_CLIENT_SECRET or "";
 }
