0ced9071db94434d7f06865813ccac923fc328cf
Dan <vareladanmarj@gmail.com>
2026-09-14 00:39:07 +0800

    fix: migration

--- drizzle/0012_gray_vivisector.sql
+++ drizzle/0012_gray_vivisector.sql
@@ -8,7 +8,7 @@ CREATE TABLE `__new_user` (
 	`deleted_at` integer
 );
 --> statement-breakpoint
-INSERT INTO `__new_user`("id", "display_name", "email", "avatar_storage_key", "avatar_mime", "deleted_at") SELECT "id", "display_name", "email", "avatar_storage_key", "avatar_mime", "deleted_at" FROM `user`;--> statement-breakpoint
+INSERT INTO `__new_user`("id", "display_name", "email", "avatar_storage_key", "avatar_mime") SELECT "id", "display_name", "email", "avatar_storage_key", "avatar_mime" FROM `user`;--> statement-breakpoint
 DROP TABLE `user`;--> statement-breakpoint
 ALTER TABLE `__new_user` RENAME TO `user`;--> statement-breakpoint
 PRAGMA foreign_keys=ON;--> statement-breakpoint
